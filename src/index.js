1ed5f7fa6914900fa797c15d1ca4cbbd0a01cf28
blohamen <staver82@gmail.com>
2017-09-11 20:26:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
+var len = (+first) + (+second) > 10000 ? len = 10000 : len = (+first) + (+second) ,
+	result = Array(len).fill(0);
+first =  first.split('')
+		.reverse();
+second = second.split('')
+		.reverse();
+for (var i = 0; i< first.length; i++){
+	for (var j = 0; j< second.length; j++){
+			result[i + j] += first[i] * second[j];
+	}
+}
+for(var i = 0 ; i < len-1 ; i++){
+if (result[i] >= 10) {
+if (!result[i + 1]) {
+result[i + 1] = 0;
+		}
+
+result[i + 1] += parseInt(result[i] / 10);
+result[i] %= 10;
+	}
+}
+
+return result.reverse()
+	.join('')
+	.replace(/^0+/, '')
 }
